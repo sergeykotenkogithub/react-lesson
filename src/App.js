ecec19b3d275c66a1a427a5cd4ab9d72b3d811e9
sergeykotenko <sergeykotenko@bk.ru>
2022-06-27 16:02:13 +0300

    lesson 5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {useDispatch, useSelector} from 'react-redux';
 
 function App() {
+  const count = useSelector(state => state.count)
+  const dispatch = useDispatch();
+
+  const increase = () => {
+    dispatch({type: 'plus'})
+  }
+  const decrease = () => {
+    dispatch({type: 'minus'})
+  }
+  const reset = () => {
+    dispatch({type: 'reset'})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={increase}>+</button>
+      <div>{count}</div>
+      <button onClick={decrease}>-</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
